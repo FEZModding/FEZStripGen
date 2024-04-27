bbfa0a0d8b83eca8d48ecf1e26fedc73095801dc
Krzyhau <krzyht@gmail.com>
2024-04-27 17:58:41 +0200

    exclude reference to unique game types

--- Stripper.cs
+++ Stripper.cs
@@ -11,6 +11,11 @@ namespace FEZStripGen
             {
                 StripType(type);
             }
+
+            RemoveUncommonTypes(module, new[]{
+                "FezGame.Components.MockAchievement",
+                "FezGame.Services.MockUser",
+            });
         }
 
         private static void StripType(TypeDefinition type)
@@ -30,6 +35,32 @@ namespace FEZStripGen
                 if (method.HasBody && method.Body.Instructions.Count != 0)
                 {
                     method.Body = new MethodBody(method);
+                }
+            }
+        }
+
+        private static void RemoveUncommonTypes(ModuleDefinition module, string[] namesOfTypesToRemove)
+        {
+            foreach(var type in module.Types)
+            {
+                // clean content of types that should be removed,
+                // so they're not used to generate hooks
+                if (namesOfTypesToRemove.Contains(type.FullName))
+                {
+                    type.Methods.Clear();
+                }
+
+                // Some methods will still reference them.
+                // Make sure they're removed as well.
+                var methodsToRemove = type.Methods.Where(
+                    method => method.Parameters.Any(
+                        parameter => namesOfTypesToRemove.Contains(parameter.ParameterType.FullName)
+                    )
+                ).ToList();
+
+                foreach(var method in methodsToRemove)
+                {
+                    type.Methods.Remove(method);
                 }
             }
         }
